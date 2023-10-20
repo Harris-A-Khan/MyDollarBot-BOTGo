49ae7a375729175d88fe522e6f02d677926597a6
Sarvesh-Somasundaram <ssomasu@ncsu.edu>
2023-10-19 20:14:51 -0400

    [Feature} api routes for frontend data population

--- frontend/mydollarbot-frontend/src/api/getData.ts
+++ frontend/mydollarbot-frontend/src/api/getData.ts
@@ -2,3 +2,13 @@ export async function getData(): Promise<Response> {
   const data = await fetch('/api/dummyData')
   return data;
 }
+
+export async function getBudgetData(): Promise<Response> {
+  const data = await fetch('/api/all-budget-data')
+  return data;
+}
+
+export async function getCategoryData(): Promise<Response> {
+  const data = await fetch('/api/all-categories')
+  return data;
+}
